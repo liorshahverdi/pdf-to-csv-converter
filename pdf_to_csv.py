a54e10a68472041ed9b854dd4ce3637931173e02
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-27 15:28:45 -0700

    converts properly! other than biden since he was formatted oddly

--- pdf_to_csv.py
+++ pdf_to_csv.py
@@ -1,49 +1,57 @@
-import requests
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from cStringIO import StringIO
+from subprocess import call
+from sys import argv
+import pandas as pd 
 
-def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = file(path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos=set()
+def transform(filename):
+	call(["pdftotext", "-layout", filename])
+	return filename.split(".")[0]+".txt"
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
+def segment(contents):
+	relevant = []
+	start = False
+	for line in contents:
+		if "Vice" in line:
+			start = True
+		if "Published" in line:
+			start = False
+		if start:
+			relevant.append(line)
+	return relevant
 
-    text = retstr.getvalue()
+def parse(relevant):
+	tmp = {}
+	tmp1 = {}
+	df = pd.DataFrame()
+	for line in relevant:
+		split_up = line.split(" ")
+		split_up = [elem for elem in split_up if elem != '']
+		if len(split_up) == 5:
+			tmp['last_name'] = split_up[0]
+			tmp['first_name'] = split_up[1]
+			tmp['party-state'] = split_up[2]
+			tmp['suite'] = split_up[3]
+			tmp['phone'] = split_up[4]
+			df = df.append(tmp, ignore_index=True)
+		elif len(split_up) == 10:
+			tmp['last_name'] = split_up[0]
+			tmp['first_name'] = split_up[1]
+			tmp['party-state'] = split_up[2]
+			tmp['suite'] = split_up[3]
+			tmp['phone'] = split_up[4]
+			tmp1['last_name'] = split_up[5]
+			tmp1['first_name'] = split_up[6]
+			tmp1['party-state'] = split_up[7]
+			tmp1['suite'] = split_up[8]
+			tmp1['phone'] = split_up[9]
+			df = df.append(tmp, ignore_index=True)
+			df = df.append(tmp1, ignore_index=True)
+	return df
 
-    fp.close()
-    device.close()
-    retstr.close()
-    return text
-
-# write pdf to a file
-url = 'http://www.prb.org/pdf14/2014-world-population-data-sheet_eng.pdf'
-res = requests.get(url)
-res.raise_for_status()
-new_pdf_filename = 'population_data_sheet.pdf'
-new_pdf = open(new_pdf_filename, 'wb')
-for chnk in res.iter_content(100000):
-	new_pdf.write(chnk)
-new_pdf.close()
-print 'pdf has been downloaded.'
-
-file_text = convert_pdf_to_txt('population_data_sheet.pdf')
-ftl = file_text.split()
-
-newfile = open('out.txt', 'wb')
-for x in ftl:
-	newfile.write(x)
-	newfile.write('\n')
-print 'Done.'
+if __name__ == '__main__':
+	txt_file = transform(argv[1])
+	text = open(txt_file, "r").read().decode("ascii", "ignore")
+	contents = text.split('\n')
+	relevant = segment(contents)
+	df = parse(relevant)
+	df.to_csv("results.csv")
+	print 'csv file success'
