327d6b25cdd523c3115570e2e7cc7b4fe8ea2d8a
LiorUbuntu <liorshahverdi@gmail.com>
2015-08-27 09:05:31 -0700

    parse and write text to file

--- extractor.py
+++ extractor.py
@@ -1,4 +1,3 @@
-import requests
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -28,22 +27,4 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
-# write pdf to a file
-url = 'http://www.prb.org/pdf14/2014-world-population-data-sheet_eng.pdf'
-res = requests.get(url)
-res.raise_for_status()
-new_pdf_filename = 'population_data_sheet.pdf'
-new_pdf = open(new_pdf_filename, 'wb')
-for chnk in res.iter_content(100000):
-	new_pdf.write(chnk)
-new_pdf.close()
-print 'pdf has been downloaded.'
-
-file_text = convert_pdf_to_txt('population_data_sheet.pdf')
-ftl = file_text.split()
-
-newfile = open('out.txt', 'wb')
-for x in ftl:
-	newfile.write(x)
-	newfile.write('\n')
-print 'Done.'
+print convert_pdf_to_txt('population_data_sheet.pdf')
